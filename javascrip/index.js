826f477d4f167f1f4b4508e1b6982c59e02676b3
tiancai <sky0975657713@gmail.com>
2021-05-16 00:08:53 +0800

    20210515-5

--- javascrip/index.js
+++ javascrip/index.js
@@ -1,5 +1,4 @@
 document.querySelector('.header').style.width=window.innerWidth+"px";
 window.onresize=function(){
-    document.querySelector('.header').style.width=window.innerWidth+"px";
+    document.querySelector('.header').style.width=window.innerWidth+"px";  
 }
-    
