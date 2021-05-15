c72524d802ce79d423cef3d4cbb61f8babe3a348
tiancai <sky0975657713@gmail.com>
2021-05-15 23:01:44 +0800

    20210515-3

---
+++ javascrip/index.js
@@ -0,0 +1,5 @@
+document.querySelector('.header').style.width=window.innerWidth+"px";
+window.onresize=function(){
+    document.querySelector('.header').style.width=window.innerWidth+"px";
+}
+    
