c63d4d001c6d98b42918004eafebbc1cc37a8a6f
tiancai <sky0975657713@gmail.com>
2021-06-15 20:18:44 +0800

    vue

--- side project/coffe/all.js/member.js
+++ side project/coffe/all.js/member.js
@@ -1,26 +1,47 @@
-const reg =document.querySelector('.reg')
-const log =document.querySelector('.log')
-const regcont =document.querySelector('.reg-content')
-const logcont =document.querySelector('.log-content')
+var vm = new Vue({
+    el:'.memberALL',
+    data:{
+        logo:'coffee!',
+        menu:['首頁', '產品列表' , '會員中心'],
+        pageLink:{
+            index:'index.html',
+            product:'product.html',
+            member:'member.html'
+        },
+        socialLink:{
+            fb:'https://facebook.com/tiancai1228',
+            ig:'https://www.instagram.com/tiancai_1228',
+            github:'https://github.com/tiancai-1228'
+        },
+        log:{
+            class:'log-content',
+            title:'會員登入',
+            color:'black',
+            type:true,
+        },
+        reg:{
+            class:'reg-content',
+            title:'會員註冊',
+            color:'gray',
+            type:false,
+        },
+        email:'sky0975657713@gmail.com',
 
+    },
+    methods:{
+        golog(){
+            
+            this.log.color='black';
+            this.reg.color='gray';
+            this.log.type=true;
+            this.reg.type=false;
+        },
+        goreg(e){
+            this.log.color='gray';
+            this.reg.color='black';
+            this.log.type=false;
+            this.reg.type=true;
+        }
+    },
 
-function golog(e){
-    e.preventDefault();
-    log.style.color = 'black';
-    reg.style.color = 'gray';
-    regcont.style.display="none";
-    logcont.style.display="flex";
-}
-
-function goreg(e){
-    e.preventDefault();
-    log.style.color = 'gray';
-    reg.style.color = 'black';
-    logcont.style.display="none";
-    regcont.style.display="flex";
-}
-
-
-
-log.addEventListener('click',golog)
-reg.addEventListener('click',goreg)
+})
