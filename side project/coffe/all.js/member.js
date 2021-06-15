262d2d1d37730de25788b4461b88e25f00518407
tiancai <sky0975657713@gmail.com>
2021-06-15 21:42:26 +0800

    登入驗證

--- side project/coffe/all.js/member.js
+++ side project/coffe/all.js/member.js
@@ -3,6 +3,10 @@ var vm = new Vue({
     data:{
         logo:'coffee!',
         menu:['首頁', '產品列表' , '會員中心'],
+        account:'',
+        password:'',
+        Newaccount:'',
+        Newpassword:'',
         pageLink:{
             index:'index.html',
             product:'product.html',
@@ -14,16 +18,20 @@ var vm = new Vue({
             github:'https://github.com/tiancai-1228'
         },
         log:{
+            type:true,
             class:'log-content',
             title:'會員登入',
             color:'black',
-            type:true,
+            Noaccount :false,
+            Nopassword:false,
         },
         reg:{
+            type:false,
             class:'reg-content',
             title:'會員註冊',
             color:'gray',
-            type:false,
+            Noaccount :false,
+            Nopassword:false,
         },
         email:'sky0975657713@gmail.com',
 
@@ -36,11 +44,47 @@ var vm = new Vue({
             this.log.type=true;
             this.reg.type=false;
         },
-        goreg(e){
+        goreg(){
             this.log.color='gray';
             this.reg.color='black';
             this.log.type=false;
             this.reg.type=true;
+        },
+        logcheck(e){
+            if(this.account ==''&& this.password==''){
+                e.preventDefault();
+                this.log.Noaccount =true;
+                this.log.Nopassword=true;
+            }else if(this.account ==''){
+                e.preventDefault();
+                this.log.Noaccount =true;
+                this.log.Nopassword=false;
+            }else if(this.password==''){
+                e.preventDefault();
+                this.log.Noaccount =false;
+                this.log.Nopassword=true;
+            }else{
+                this.log.Noaccount =false;
+                this.log.Nopassword=false;
+            }
+        },
+        regcheck(e){
+            if(this.Newaccount ==''&& this.Newpassword==''){
+                e.preventDefault();
+                this.reg.Noaccount =true;
+                this.reg.Nopassword=true;
+            }else if(this.Newaccount ==''){
+                e.preventDefault();
+                this.reg.Noaccount =true;
+                this.reg.Nopassword=false;
+            }else if(this.Newpassword==''){
+                e.preventDefault();
+                this.reg.Noaccount =false;
+                this.reg.Nopassword=true;
+            }else{
+                this.reg.Noaccount =false;
+                this.reg.Nopassword=false;
+            }
         }
     },
 
